3093f50d9b963002869500b456856d322d40149e
Brandon Nguyen <brantn10@uci.edu>
2022-02-27 01:52:31 -0800

    Initial Commit

---
+++ Just Women Just Walk/Just_Women_Just_WalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_Women_Just_WalkApp.swift
+//  Just Women Just Walk
+//
+//  Created by Brandon Nguyen on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Just_Women_Just_WalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
